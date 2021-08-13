38fb05df1afe0f426f7f9d821bbc9c6a7b655183
Sai Ganesh Kodipelly <ganeshrao9949@gmail.com>
2021-08-13 09:58:26 +0530

    remove logs

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,17 +1,16 @@
 import React from 'react'
 import './Footer.css'
-function Footer() {
-    return (
-        <div>
-        
-<footer class="page-footer font-small blue pt-4" >
-  <div class="footer-copyright text-center py-3">© 2021 Copyright:
-    <a href="/"> Z-coins</a>
-  </div>
 
-</footer>
-</div>
-    )
+function Footer() {
+  return (
+    <div>
+      <footer className="page-footer font-small blue pt-4" >
+        <div className="footer-copyright text-center py-3">© 2021 Copyright:
+          <a href="/"> Z-coins</a>
+        </div>
+      </footer>
+    </div>
+  )
 }
 
 export default Footer
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Navbar, Nav, Container ,NavDropdown} from 'react-bootstrap'
+import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap'
 import { connect } from "react-redux";
 import axios from "axios"
 
@@ -15,30 +15,30 @@ function Navbarr(props) {
             console.log(err)
          })
    }
-   console.log("In nav", props)
+
    return (
       <div>
-      <Navbar className="shadow-lg bg-white rounded" collapseOnSelect bg="light" expand="lg">
-      <Container>
-      <Navbar.Brand href="#home" className="nav-title">Z-Coins</Navbar.Brand>
-         <Nav className="me-auto">
-            <Nav.Link href="#features"></Nav.Link>
-            <Nav.Link href="#pricing"></Nav.Link>
-            
-         </Nav>
-         <Nav>
-         {!props.userValue.name? (
+         <Navbar className="shadow-lg bg-white rounded" collapseOnSelect bg="light" expand="lg">
+            <Container>
+               <Navbar.Brand href="#home" className="nav-title">Z-Coins</Navbar.Brand>
+               <Nav className="me-auto">
+                  <Nav.Link href="#features"></Nav.Link>
+                  <Nav.Link href="#pricing"></Nav.Link>
+
+               </Nav>
+               <Nav>
+                  {!props.userValue.name ? (
                      <>
                         <Nav.Link href="/auth/login" className="Loginbtn">Login</Nav.Link>
                      </>
-                  ):(               
+                  ) : (
                      <NavDropdown title={props.userValue.name} id="basic-nav-dropdown">
-                     <NavDropdown.Item onClick={handleClick}>Logout</NavDropdown.Item>
-                  </NavDropdown>
-                  )} 
-         </Nav>
-      </Container>
-      </Navbar>
+                        <NavDropdown.Item onClick={handleClick}>Logout</NavDropdown.Item>
+                     </NavDropdown>
+                  )}
+               </Nav>
+            </Container>
+         </Navbar>
       </div>
 
    )
