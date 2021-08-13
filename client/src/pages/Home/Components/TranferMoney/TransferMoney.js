49fb937d2c6b85c1d241426710b655f5fed0763a
Rehasree <rehasreekoneru@gmail.com>
2021-08-13 19:39:12 +0530

    added transfer coins

--- client/src/pages/Home/Components/TranferMoney/TransferMoney.js
+++ client/src/pages/Home/Components/TranferMoney/TransferMoney.js
@@ -4,9 +4,24 @@ import Modal from '../../../../components/Modal/Modal';
 import './TransferMoney.css'
 import axios from 'axios'
 
-function TransferMoney() {
+function TransferMoney(props) {
     const [phoneNumber, setPhoneNumber] = useState("")
     const [coins, setCoins] = useState(0)
+    const trasferCoins=(e)=>{
+        e.preventDefault()
+        console.log(phoneNumber,coins)
+        if(phoneNumber.length!=10 || coins > props.userValue.coins  ){
+                alert("Please verify your mobile number and coins");
+        }
+        if (phoneNumber.length==10 && coins <= props.userValue.coins){
+           alert("Your coins are successfully trasferred 🎉🎉");
+           props.userValue.coins = props.userValue.coins- coins 
+        }
+        if(phoneNumber===props.userValue.username )
+        {
+            alert("YoU can't transfer money to yourself");
+        }
+    }
     // const handleSubmit = (phoneNumber) => {
     //     let response
     //     response = axios.get("/check-user",{username: phoneNumber})
@@ -25,13 +40,18 @@ function TransferMoney() {
     
     return (
         <div>
+            <form>
             <Container align="left">
-                <Row>
+                <p>Todal Coins: <b>{props.userValue.coins}</b></p>
+                {props.userValue.coins ?(
+                    <>
+                    <Row>
                     <Col>
                         <div className="form-floating mb-3">
                             <input type="tel" className="form-control" id="floatingInput"
                                 name="mobileNum" placeholder="Enter mobile number"
                                 onChange={e => setPhoneNumber(e.target.value)}
+                                required
                             />
                             <label className="floating-label" htmlFor="floatingInput">Enter Mobile Number</label>
                         </div>
@@ -41,6 +61,7 @@ function TransferMoney() {
                             <input type="number" className="form-control" id="floatingInput"
                                 name="coinsCount" placeholder="Enter no. of coins"
                                 onChange={e => setCoins(parseFloat(e.target.value))}
+                                required
                             />
                             <label className="floating-label" htmlFor="floatingInput">Enter No. of Coins</label>
                         </div>
@@ -48,13 +69,15 @@ function TransferMoney() {
                     </Col>
                 </Row>
                 {/*Add a button to check if the user is present or not*/}
-                {/*<button className="btn btn-primary" onClick={handleSubmit(phoneNumber)}>Verify User</button>*/}
-                <Modal button="Validate" dataTarget="#TransferMoney" id="TransferMoney"
-                    content={`You are requesting xxxxx for ${coins} coins`} password="true"
-                    phoneNumber={phoneNumber}
-                    coins={coins}
-                />
+                <button type="submit" className="get-started" onClick={trasferCoins}>Verify User</button>
+               
+                    </>
+                ):(
+                    <b><p>You don't have sufficient coins to transfer</p></b>
+                )}
+                
             </Container>
+            </form>
         </div>
     )
 }
