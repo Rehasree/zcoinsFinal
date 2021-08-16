62599c529f211c2d5990c0482811842ca205adf4
Sai Ganesh Kodipelly <ganeshrao9949@gmail.com>
2021-08-16 19:01:50 +0530

    resolve warnings from react side

--- client/src/pages/Index/Index.js
+++ client/src/pages/Index/Index.js
@@ -5,7 +5,6 @@ import home3 from './images/3.svg'
 import { Container } from 'react-bootstrap';
 import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
 import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
-import PublicIcon from '@material-ui/icons/Public';
 import NumberCounter from 'number-counter';
 import './Index.css'
 
@@ -19,7 +18,7 @@ function Index() {
 
     return (
         <div className="home" >
-              <div className="background">
+            <div className="background">
                 <div className="cube"></div>
                 <div className="cube"></div>
                 <div className="cube"></div>
@@ -28,85 +27,86 @@ function Index() {
                 <div className="cube"></div>
             </div>
             <Container>
-               <div className="row">
-                   <div align="left" className="col-sm-12 col-lg-6 order-lg-1 order-sm-2 order-xs-2">
+                <div className="row">
+                    <div align="left" className="col-sm-12 col-lg-6 order-lg-1 order-sm-2 order-xs-2">
                         <h1 >LET YOUR MONEY GROW...</h1>
                         <p className="desc d-flex flex-column align-items-stretch justify-content-center py-5 px-lg-5" >
                             Who said that you need to work hard to get ahead? Put your money in Z coins and just see it grow.
                             Z coins uses a unique technology that allows you to invest your money in the most safe and effective way.
                             The smart AI system will take decisions about where to invest, when to invest, and how to invest.
-                            Therefore all you need to is just invest and relax. 
+                            Therefore all you need to is just invest and relax.
                         </p>
-                       <a href="/auth/login"><button className="get-started">Get started <ArrowForwardIcon /> </button></a> 
-                   </div>
-                   <div className="col-sm-12 col-lg-6 order-lg-2 order-sm-1 order-xs-1">
-                    <img id="float" className="img-fluid" src={home} alt="img" />
-                   </div>
+                        <a href="/auth/login"><button className="get-started">Get started <ArrowForwardIcon /> </button></a>
+                    </div>
+                    <div className="col-sm-12 col-lg-6 order-lg-2 order-sm-1 order-xs-1">
+                        <img id="float" className="img-fluid" src={home} alt="img" />
+                    </div>
                 </div>
-                <br/><br/>
+                <br /><br />
                 <div className="row" >
-                {data.map((info, index) => {
+                    {data.map((info, index) => {
                         return (
                             <div className="col">
                                 <div className="shadow-lg p-3 mb-5 bg-white rounded count-box">
                                     <span >
-                                       <b><NumberCounter end={info.count} delay={info.delay} className="increment" preFix={info.prefix} postFix={info.postfix} /></b> 
+                                        <b><NumberCounter end={info.count} delay={info.delay} className="increment" preFix={info.prefix} postFix={info.postfix} /></b>
                                     </span>
                                     <p>{info.title}</p>
                                 </div>
                             </div>
-                        )})}
-                    
+                        )
+                    })}
+
                 </div>
 
                 <div className="row" style={{ marginTop: "6rem" }}>
-                <>
-                    <div className="col-sm-12 col-lg-6">
-                        <img style={{ padding: "10px" }} className="img-fluid" style={{width:"400px",height:"400px"}} src={home2} alt="img" />
-                    </div>
-                    <div  className="col-sm-12 col-lg-6 content" align="left">
-                        <h1 >Smartest way of investing...</h1>
-                        <p className="desc d-flex flex-column align-items-stretch justify-content-center py-5 px-lg-5" >
-                        coins is the best way to start investing your money. Don't worry about where to invest, when to invest and how to invest.
-                            Just put your money in Z coins and let the system do the rest. Z coins will automatically invest your money in the best possible way.
-                        </p>
-                    </div>
-                </>
+                    <>
+                        <div className="col-sm-12 col-lg-6">
+                            <img style={{ padding: "10px", width: "400px", height: "400px" }} className="img-fluid" src={home2} alt="img" />
+                        </div>
+                        <div className="col-sm-12 col-lg-6 content" align="left">
+                            <h1 >Smartest way of investing...</h1>
+                            <p className="desc d-flex flex-column align-items-stretch justify-content-center py-5 px-lg-5" >
+                                coins is the best way to start investing your money. Don't worry about where to invest, when to invest and how to invest.
+                                Just put your money in Z coins and let the system do the rest. Z coins will automatically invest your money in the best possible way.
+                            </p>
+                        </div>
+                    </>
                 </div>
 
-               <div className="row" style={{background:"#FFE3FE",borderRadius:"25px",padding:"20px"}}>
-                   <div className="container">
-                       <p style={{fontSize:"35px",fontFamily:"cursive",fontWeight:"bolder",color:"#93329E"}}>"You can be possessive but don't let your money be"</p>
-                       <p className="desc d-flex flex-column align-items-stretch justify-content-center py-5 px-lg-5" >
-                            Having all your money in your wallet is not a good idea. Money loses value over time due to inflation. 
+                <div className="row" style={{ background: "#FFE3FE", borderRadius: "25px", padding: "20px" }}>
+                    <div className="container">
+                        <p style={{ fontSize: "35px", fontFamily: "cursive", fontWeight: "bolder", color: "#93329E" }}>"You can be possessive but don't let your money be"</p>
+                        <p className="desc d-flex flex-column align-items-stretch justify-content-center py-5 px-lg-5" >
+                            Having all your money in your wallet is not a good idea. Money loses value over time due to inflation.
                             In order to overcome that, you need to be smart OR you need to invest in a smart system.
-                            Z coins is a smart system that helps you overcome inflation by investing the money in stock market. 
+                            Z coins is a smart system that helps you overcome inflation by investing the money in stock market.
                         </p>
-                   </div>
-               </div>
+                    </div>
+                </div>
 
                 <div className="row">
-                <>
-                    <div align="left" className="col-sm-12 col-lg-6 order-lg-1 order-sm-2 order-xs-2">
-                        <h1 >WHY SHOULD U INVEST?</h1>
-                        <p className="desc d-flex flex-column align-items-stretch justify-content-center py-5 px-lg-5" >
-                            You need to invest your money in order to overcome the inflation and Z coins is the best place to do so. 
-                            Through Z coins, you don't need to have the knowledge of stock market of investing. You can invest your money in different low-risk stocks 
-                            using Z coins.  
-                            Z coins has proved itself by bringing good returns to its investors. Why wait when you can also be an investor?
-                            <br/>
-                            Like always, the more the merrier. 
-                        </p>
-                       <a href="/reports"> <button className="get-started">Checkout our report <ArrowForwardIcon/> </button></a>
-                    </div>
-                    <div className="col-sm-12 col-lg-6 order-lg-2 order-sm-1 order-xs-1">
-                        <img className="img-fluid" src={home3} alt="img" />
-                    </div>
-                </>
+                    <>
+                        <div align="left" className="col-sm-12 col-lg-6 order-lg-1 order-sm-2 order-xs-2">
+                            <h1 >WHY SHOULD U INVEST?</h1>
+                            <p className="desc d-flex flex-column align-items-stretch justify-content-center py-5 px-lg-5" >
+                                You need to invest your money in order to overcome the inflation and Z coins is the best place to do so.
+                                Through Z coins, you don't need to have the knowledge of stock market of investing. You can invest your money in different low-risk stocks
+                                using Z coins.
+                                Z coins has proved itself by bringing good returns to its investors. Why wait when you can also be an investor?
+                                <br />
+                                Like always, the more the merrier.
+                            </p>
+                            <a href="/reports"> <button className="get-started">Checkout our report <ArrowForwardIcon /> </button></a>
+                        </div>
+                        <div className="col-sm-12 col-lg-6 order-lg-2 order-sm-1 order-xs-1">
+                            <img className="img-fluid" src={home3} alt="img" />
+                        </div>
+                    </>
                 </div>
 
             </Container>
-        
+
         </div>
     )
 }
